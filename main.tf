1f80994e281fdc200243e0a10de042d7d86ca935
sjayanthi <sjayanthi@opentext.com>
2024-05-16 16:50:09 -0400

    module specific

--- main.tf
+++ main.tf
@@ -10,16 +10,23 @@ data "terraform_remote_state" "rg" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-    name = "sree-${var.project_code}-${var.location}-${var.environment}-vnet"
-    resource_group_name = data.terraform_remote_state.rg.outputs.resource_group_name
-    location = var.location
-    address_space = [ "10.228.0.0/26" ]
+    name = var.nw_vnet_name
+    resource_group_name =  var.nw_resource_group_name
+    location = var.nw_location
+    address_space = var.nw_address_space
+    # name = "sree-${var.project_code}-${var.location}-${var.environment}-vnet"
+    # resource_group_name = data.terraform_remote_state.rg.outputs.resource_group_name
+    # location = var.location
+    # address_space = [ "10.228.0.0/26" ]
 }
 
 resource "azurerm_subnet" "snet" {
     name = "${azurerm_virtual_network.vnet.name}-snet"
-    resource_group_name = data.terraform_remote_state.rg.outputs.resource_group_name
-    address_prefixes = [ "10.228.0.0/27" ]
+    resource_group_name = var.nw_resource_group_name
+    address_prefixes = var.nw_subnet_address_prefixes
     virtual_network_name = azurerm_virtual_network.vnet.name
-    depends_on = [ azurerm_virtual_network.vnet ]
+    # resource_group_name = data.terraform_remote_state.rg.outputs.resource_group_name
+    # address_prefixes = [ "10.228.0.0/27" ]
+    # virtual_network_name = azurerm_virtual_network.vnet.name
+    # depends_on = [ azurerm_virtual_network.vnet ]
 }
--- variables.tf
+++ variables.tf
@@ -1,14 +1,43 @@
 variable "project_code" {
-    default = "prj01"
+    default = ""
+    type = string
     description = "shortcode for project"
 }
 
 variable "location" {
-    default = "eastus"
+    default = ""
+    type = string
     description = "location where project is hosted"
 }
 
 variable "environment" {
-    default = "dev"
+    default = ""
+    type = string
     description = "host in the environment"
+}
+
+variable "nw_vnet_name" {
+    default = ""
+    description = "network name"
+    type = string
+}
+
+variable "nw_resource_group_name" {
+    type = string
+    default = ""
+}
+
+variable "nw_location" {
+    type = string
+    default = ""
+}
+
+variable "nw_address_space" {
+    type = list(string)
+    default = [ "" ]
+}
+
+variable "nw_subnet_address_prefixes" {
+    type = list(string)
+    default = [ "" ]
 }
